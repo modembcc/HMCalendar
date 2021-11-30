5020bb51dabef8116bc7622e791099a8dc1f0645
modembcc <58062202+modembcc@users.noreply.github.com>
2021-11-30 22:53:53 +0700

    Added dropdown

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -1,12 +1,27 @@
-$("select").change(function () {
-    // hide all optional elements
-    $('.optional').css('display','none');
-
-    $("select option:selected").each(function () {
-        if($(this).val() == "Referral") {
-            $('.referral').css('display','block');
-        } else if($(this).val() == "Other") {
-            $('.other').css('display','block');
-        }
+$(document).ready(function () {
+    var seas = [
+        "Error",
+        "Spring_1",
+        "Summer_1",
+        "Fall_1",
+        "Winter_1",
+        "Spring_2",
+        "Summer_2",
+        "Fall_2",
+        "Winter_2"
+      ]
+    var cur = 1;
+    for(var i=1;i<=8;i++){
+        $("#"+seas[i]).hide();
+    }
+    console.log(cur);
+    $("#"+seas[cur]).show();
+    $("#season").change(function(event) {
+        $("#season option:selected").each(function () {
+            var tmp = $(this).val();
+            $("#"+seas[cur]).hide();
+            $("#"+seas[tmp]).show();
+            cur = tmp;
+        });
     });
 });
