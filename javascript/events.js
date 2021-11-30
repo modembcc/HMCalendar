788caff08aa1abe8d651d4ba2b189ed2a657e319
modembcc <58062202+modembcc@users.noreply.github.com>
2021-11-30 21:16:25 +0700

    Added new js for inserting events

---
+++ javascript/events.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var days = [
+      "Error",
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    var seas = [
+      "Error",
+      "Spring_1",
+      "Summer_1",
+      "Fall_1",
+      "Winter_1",
+      "Spring_2",
+      "Summer_2",
+      "Fall_2",
+      "Winter_2"
+    ]
+  });
+  
